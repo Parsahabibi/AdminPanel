9f7f25dd961ce53d2304d8bb6f0ecb377243c044
Parsa <parsa.habibi.2002@gmail.com>
2023-10-05 12:14:44 +0330

    refactor:Matte screen was added to the back of the drawer

--- assets/js/HomePage.js
+++ assets/js/HomePage.js
@@ -1,32 +1,10 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     let hamburgerMenu = document.querySelector('.hamburgerMenu');
-//     let dashboard = document.querySelector('.Dashboard');
-//
-//     hamburgerMenu.addEventListener('click', function(event) {
-//         if (dashboard.style.display === 'none' || dashboard.style.display === '') {
-//             dashboard.style.display = 'block';
-//             dashboard.style.position = 'absolute'
-//             dashboard.style.zIndex ='10';
-//         } else {
-//             dashboard.style.display = 'none';
-//         }
-//         event.stopPropagation();
-//     });
-//     document.addEventListener('click', function(event) {
-//         if (!dashboard.contains(event.target) && !hamburgerMenu.contains(event.target)) {
-//             dashboard.style.display = 'none';
-//         }
-//     });
-//
-//     dashboard.addEventListener('click', function(event) {
-//         event.stopPropagation();
-//     });
-// });
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
     let hamburgerMenu = document.querySelector('.hamburgerMenu');
     let dashboard = document.querySelector('.Dashboard');
+    let overlay = document.querySelector('.overlay');
 
     hamburgerMenu.addEventListener('click', function (event) {
         if (window.innerWidth <= 900) {
@@ -34,8 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 dashboard.style.position = 'absolute';
                 dashboard.style.zIndex = '10';
                 dashboard.classList.remove('visible');
+                overlay.classList.remove('visible');
             } else {
                 dashboard.classList.add('visible');
+                overlay.classList.add('visible');
             }
         }
         event.stopPropagation();
@@ -45,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (window.innerWidth <= 900) {
             if (!dashboard.contains(event.target) && !hamburgerMenu.contains(event.target)) {
                 dashboard.classList.remove('visible');
+                overlay.classList.remove('visible');
             }
         }
     });
