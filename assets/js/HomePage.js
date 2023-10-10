5aea8b4c7f40e819368bd48a9755eadf1884bf52
Parsa <parsa.habibi.2002@gmail.com>
2023-10-10 22:01:04 +0330

    Weekly Income for Mobile and Desktop with ChartJs done

--- assets/js/HomePage.js
+++ assets/js/HomePage.js
@@ -1,4 +1,8 @@
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
+
 
     function convertToPersianNumber(input) {
         const persianDigits = '۰۱۲۳۴۵۶۷۸۹';
@@ -42,4 +46,71 @@ document.addEventListener("DOMContentLoaded", function () {
     dashboard.addEventListener('click', function (event) {
         event.stopPropagation();
     });
+
+
+    let ctx = document.getElementById('myBarChart').getContext('2d');
+
+    let myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['19', '20', '21', '22', '23', '24', '25'],
+            datasets: [{
+                label: 'بخش 1',
+                data: [5, 10, 15, 20, 25, 30, 35],
+                backgroundColor: '#775FFC'
+            }, {
+                label: 'بخش 2',
+                data: [3, 6, 9, 12, 15, 18, 21],
+                backgroundColor: '#6AD2FF'
+            }, {
+                label: 'بخش 3',
+                data: [2, 4, 6, 8, 10, 12, 14],
+                backgroundColor: '#EFF4FB'
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    stacked: true,
+                    gridLines: {
+                        drawBorder: false,
+                        display: false,
+                        drawOnChartArea: false
+                    },
+                    grid: {
+                        display: false
+                    },
+                    ticks: {
+                        display: false,
+                        fontSize: 14,
+                    }
+                },
+                x: {
+                    stacked: true,
+                    barThickness: 17,
+                    barPercentage: 0,
+                    gridLines: {
+                        drawBorder: false,
+                        display: false
+                    },
+                    grid: {
+                        display: false
+                    },
+
+                },
+
+            }
+        }
+
+    });
+
+
 });
