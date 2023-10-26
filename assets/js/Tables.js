b68769b4b0f2d0203549f47f52dba7dbee031f24
Parsa <parsa.habibi.2002@gmail.com>
2023-10-26 12:12:36 +0330

    DevelopTable for Mobile and Desktop Done and ReviewTable fixed

--- assets/js/Tables.js
+++ assets/js/Tables.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-//start ReviewTable table js
+//start ReviewTable js
 
 
 let rowData = [
@@ -62,7 +62,6 @@ let rowData = [
         count: "2.458",
         date: "24.فرو.1402"
     },
-
     {
         isChecked: false,
         label: "لورم ایپسوم",
@@ -70,8 +69,55 @@ let rowData = [
         count: "2.458",
         date: "24.فرو.1402"
     },
-
-
+    {
+        isChecked: true,
+        label: "لورم ایپسوم",
+        progress: "17.5%",
+        count: "2.458",
+        date: "24.فرو.1402"
+    },
+    {
+        isChecked: true,
+        label: "لورم ایپسوم",
+        progress: "17.5%",
+        count: "2.458",
+        date: "24.فرو.1402"
+    },
+    {
+        isChecked: false,
+        label: "لورم ایپسوم",
+        progress: "17.5%",
+        count: "2.458",
+        date: "24.فرو.1402"
+    },
+    {
+        isChecked: false,
+        label: "لورم ایپسوم",
+        progress: "17.5%",
+        count: "2.458",
+        date: "24.فرو.1402"
+    },
+    {
+        isChecked: true,
+        label: "لورم ایپسوم",
+        progress: "17.5%",
+        count: "2.458",
+        date: "24.فرو.1402"
+    },
+    {
+        isChecked: true,
+        label: "لورم ایپسوم",
+        progress: "17.5%",
+        count: "2.458",
+        date: "24.فرو.1402"
+    },
+    {
+        isChecked: true,
+        label: "لورم ایپسوم",
+        progress: "17.5%",
+        count: "2.458",
+        date: "24.فرو.1402"
+    },
     {
         isChecked: true,
         label: "لورم ایپسوم",
@@ -83,6 +129,7 @@ let rowData = [
 
 
 function generateTableRows(data, targetTableBody) {
+    let counter = 1;
     data.forEach(item => {
         let tr = document.createElement("tr");
 
@@ -90,12 +137,18 @@ function generateTableRows(data, targetTableBody) {
         tdInput.className = "Input";
         let div = document.createElement("div");
         div.className = "InputReviewTable";
+
+        let uniqueID = "InputReviewTable" + counter;
+
+
+
         let input = document.createElement("input");
-        input.id = "InputReviewTable";
+        input.id = uniqueID;
         input.type = "checkbox";
         input.checked = item.isChecked;
         let label = document.createElement("label");
-        label.htmlFor = "InputReviewTable";
+        label.htmlFor = uniqueID;
+        label.appendChild(document.createTextNode(item.label));
         label.textContent = item.label;
         div.appendChild(input);
         div.appendChild(label);
@@ -118,12 +171,167 @@ function generateTableRows(data, targetTableBody) {
         tr.appendChild(tdDate);
 
         targetTableBody.appendChild(tr);
+        counter++;
     });
 }
 
 
 let targetTableBody = document.querySelector("tbody");
 generateTableRows(rowData, targetTableBody);
+
+
+
+//start DevelopTable
+
+
+let DevelopTable = [
+    {
+        title: "لورم ایپسوم",
+        systems: ["../assets/image/Windows.svg", "../assets/image/android.svg", "../assets/image/Apple.svg"],
+        date: "24.فرور.1402",
+        progressValue: "75.5%",
+        progressPercentage: "75.5%"
+    },
+    {
+        title: "لورم ایپسوم",
+        systems: ["../assets/image/Windows.svg", "../assets/image/android.svg", "../assets/image/Apple.svg"],
+        date: "24.فرور.1402",
+        progressValue: "75.5%",
+        progressPercentage: "75.5%"
+    },
+    {
+        title: "لورم ایپسوم",
+        systems: ["../assets/image/Windows.svg", "../assets/image/android.svg", "../assets/image/Apple.svg"],
+        date: "24.فرور.1402",
+        progressValue: "75.5%",
+        progressPercentage: "75.5%"
+    },
+    {
+        title: "لورم ایپسوم",
+        systems: ["../assets/image/Windows.svg", "../assets/image/android.svg", "../assets/image/Apple.svg"],
+        date: "24.فرور.1402",
+        progressValue: "75.5%",
+        progressPercentage: "75.5%"
+    },
+    {
+        title: "لورم ایپسوم",
+        systems: ["../assets/image/Windows.svg", "../assets/image/android.svg", "../assets/image/Apple.svg"],
+        date: "24.فرور.1402",
+        progressValue: "75.5%",
+        progressPercentage: "75.5%"
+    },
+    {
+        title: "لورم ایپسوم",
+        systems: ["../assets/image/Windows.svg", "../assets/image/android.svg", "../assets/image/Apple.svg"],
+        date: "24.فرور.1402",
+        progressValue: "75.5%",
+        progressPercentage: "75.5%"
+    },
+    {
+        title: "لورم ایپسوم",
+        systems: ["../assets/image/Windows.svg", "../assets/image/android.svg", "../assets/image/Apple.svg"],
+        date: "24.فرور.1402",
+        progressValue: "75.5%",
+        progressPercentage: "75.5%"
+    },
+    {
+        title: "لورم ایپسوم",
+        systems: ["../assets/image/Windows.svg", "../assets/image/android.svg", "../assets/image/Apple.svg"],
+        date: "24.فرور.1402",
+        progressValue: "75.5%",
+        progressPercentage: "75.5%"
+    },
+    {
+        title: "لورم ایپسوم",
+        systems: ["../assets/image/Windows.svg", "../assets/image/android.svg", "../assets/image/Apple.svg"],
+        date: "24.فرور.1402",
+        progressValue: "75.5%",
+        progressPercentage: "75.5%"
+    },
+    {
+        title: "لورم ایپسوم",
+        systems: ["../assets/image/Windows.svg", "../assets/image/android.svg", "../assets/image/Apple.svg"],
+        date: "24.فرور.1402",
+        progressValue: "75.5%",
+        progressPercentage: "75.5%"
+    },
+    {
+        title: "لورم ایپسوم",
+        systems: ["../assets/image/Windows.svg", "../assets/image/android.svg", "../assets/image/Apple.svg"],
+        date: "24.فرور.1402",
+        progressValue: "75.5%",
+        progressPercentage: "75.5%"
+    },
+
+];
+
+function generateDevelopTableRows(data, targetTableBody) {
+    data.forEach(item => {
+        let tr = document.createElement("tr");
+
+        // Title Column
+        let tdTitle = document.createElement("td");
+        tdTitle.className = "TitleDevelopTable";
+        tdTitle.textContent = item.title;
+        tr.appendChild(tdTitle);
+
+        // System Icons Column
+        let tdSystem = document.createElement("td");
+        tdSystem.className = "SystemDevelopTable";
+        let divSystem = document.createElement("div");
+        divSystem.className = "RowSystem";
+        item.systems.forEach(src => {
+            let img = document.createElement("img");
+            img.src = src;
+            img.alt = "";
+            divSystem.appendChild(img);
+        });
+        tdSystem.appendChild(divSystem);
+        tr.appendChild(tdSystem);
+
+        // Date Column
+        let tdDate = document.createElement("td");
+        tdDate.className = "DateDevelopTable";
+        tdDate.textContent = item.date;
+        tr.appendChild(tdDate);
+
+        // Progress Column
+        let tdProgress = document.createElement("td");
+        tdProgress.className = "ProgressDevelopTable";
+
+        let h6 = document.createElement("h6");
+        h6.textContent = item.progressValue;
+
+        let p = document.createElement("p");
+        p.textContent = item.progressPercentage;
+
+        let divProgress = document.createElement("div");
+        divProgress.className = "Progress";
+
+        let divComplete = document.createElement("div");
+        divComplete.className = "Complete";
+
+        let percentageValue = parseFloat(item.progressValue.replace('%', ''));
+        divComplete.style.width = `${percentageValue}%`;
+
+        divProgress.appendChild(divComplete);
+        tdProgress.appendChild(h6);
+        tdProgress.appendChild(p);
+        tdProgress.appendChild(divProgress);
+
+        tr.appendChild(tdProgress);
+        targetTableBody.appendChild(tr);
+    });
+}
+
+let targetDevelopTableBody = document.querySelector(".Develop");
+if (targetDevelopTableBody) {
+    generateDevelopTableRows(DevelopTable, targetDevelopTableBody);
+} else {
+    console.error("Element '.Develop tbody' not found.");
+}
+
+
 
 
 
