9d4cf5dc73c4079cae2a0cd8eabf5c5e30eb9475
Parsa <parsa.habibi.2002@gmail.com>
2023-11-05 15:31:37 +0330

    Refactor: The password change page Fixed and Change Page after Confirm Password

--- assets/js/MobileChangePassword.js
+++ assets/js/MobileChangePassword.js
@@ -51,13 +51,18 @@ document.querySelector('.MobileConfirm').addEventListener('click', function () {
     const currentPasswordInput = document.getElementById('current').value;
     const newPasswordInput = document.getElementById('NewPassword').value;
     const repeatPasswordInput = document.getElementById('RepeatPassword').value;
+    let ButtonLink = document.getElementById('ButtonLink');
 
     if (currentPasswordInput === "") {
-        showCustomNotification( "رمز عبور فعلی را وارد کنید");
+        MobileShowCustomNotification( "رمز عبور فعلی را وارد کنید");
+        console.log('1')
+
     } else if (newPasswordInput !== repeatPasswordInput) {
-        showCustomNotification("رمز عبور و تکرار رمز عبور باید یکسان باشند")
+        MobileShowCustomNotification("رمز عبور و تکرار رمز عبور باید یکسان باشند")
+        console.log('2')
+
     } else {
-        window.location.href = 'صفحه-دیگر.html';
+        ButtonLink.href = 'HomePage.html'
         document.getElementById('current').value = "";
         document.getElementById('NewPassword').value = "";
         document.getElementById('RepeatPassword').value = "";
@@ -70,13 +75,14 @@ document.querySelector('.Confirm').addEventListener('click', function () {
     let currentDesktopPasswordInput = document.getElementById('DesktopCurrent').value;
     let newDesktopPasswordInput = document.getElementById('DesktopNewPassword').value;
     let repeatDesktopPasswordInput = document.getElementById('RepeatDesktopPassword').value;
+    let DesktopButtonLink = document.getElementById('DesktopButtonLink');
 
     if (currentDesktopPasswordInput === "") {
         showCustomNotification( "رمز عبور فعلی را وارد کنید");
     } else if (newDesktopPasswordInput !== repeatDesktopPasswordInput) {
         showCustomNotification("رمز عبور و تکرار رمز عبور باید یکسان باشند")
     } else {
-        window.location.href = '../../../AltynAdmin/pages/HomePage.html';
+        DesktopButtonLink.href = 'HomePage.html'
         document.getElementById('DesktopCurrent').value = "";
         document.getElementById('DesktopNewPassword').value = "";
         document.getElementById('RepeatDesktopPassword').value = "";
@@ -88,6 +94,19 @@ function showCustomNotification(message) {
 
     let notification = document.getElementById('DesktopCustomNotification');
     let notificationMessage = document.getElementById('DesktopNotificationMessage');
+
+    notificationMessage.textContent = message;
+    notification.classList.add('show-notification');
+
+    setTimeout(function () {
+        notification.classList.remove('show-notification');
+    }, 3000);
+}
+
+function MobileShowCustomNotification(message) {
+
+    let notification = document.getElementById('customNotification');
+    let notificationMessage = document.getElementById('notificationMessage');
 
     notificationMessage.textContent = message;
     notification.classList.add('show-notification');
