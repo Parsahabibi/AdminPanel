cd25eafec1aaa092d88cda3c3512363af023716f
Parsa <parsa.habibi.2002@gmail.com>
2023-11-05 17:01:13 +0330

    Refactor: The Login Page Fixed and Change Page after Confirm Email and Password

--- assets/js/MobileLoginPage.js
+++ assets/js/MobileLoginPage.js
@@ -1,11 +1,15 @@
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
 
 
     let emailInput = document.getElementById('EmailInput');
     let passwordInput = document.getElementById('PasswordInput');
+    let DesktopEmailInput = document.getElementById('DesktopEmailInput');
+    let DesktopPasswordInput = document.getElementById('DesktopPasswordInput');
+    let DesktopButtonLink = document.getElementById('DesktopSubmitButton');
+    let DesktopLinkButton = document.getElementById('DesktopEnterButton')
+
+    let MobileLinkButton = document.getElementById('MobileEnterButton')
+
 
 
     document.getElementById('TogglePasswordVisibility').addEventListener('click', function () {
@@ -18,35 +22,53 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    let formArray = [];
+    document.getElementById('DesktopTogglePasswordVisibility').addEventListener('click', function () {
+        let passwordInput = document.getElementById('DesktopPasswordInput');
 
-    document.getElementById('SubmitButton').addEventListener('click', function () {
-        saveFormData();
-        checkData();
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+        } else {
+            passwordInput.type = 'password';
+        }
     });
 
-    function saveFormData() {
+    let formArray = [];
 
 
-        let emailValue = emailInput.value;
+    document.querySelector('.SubmitButton').addEventListener('click', function () {
 
-        if (!isValidEmail(emailValue)) {
-            showCustomNotification('ایمیل وارد شده نامعتبر است');
-            return;
+        let EmailInput = document.getElementById('EmailInput').value;
+        let PasswordInput = document.getElementById('PasswordInput').value;
+        console.log(DesktopEmailInput , DesktopPasswordInput)
+
+
+        if (EmailInput === "" || PasswordInput === "") {
+            showCustomNotification( "لطقا فرم ها را کامل پر کنید");
+        } else if (isValidEmail(EmailInput) === false) {
+            showCustomNotification("ایمیل وارد شده نا معتبر است !")
+        } else {
+            checkData();
         }
+    });
 
-        let formData = {
-            email: emailInput.value,
-            password: passwordInput.value
-        };
 
-        formArray.push(formData);
 
-        console.log(formArray);
+    document.querySelector('.DesktopSubmitButton').addEventListener('click', function () {
 
-        // emailInput.value = '';
-        // passwordInput.value = '';
-    }
+        let DesktopEmailInput = document.getElementById('DesktopEmailInput').value;
+        let DesktopPasswordInput = document.getElementById('DesktopPasswordInput').value;
+        console.log(DesktopEmailInput , DesktopPasswordInput)
+
+
+        if (DesktopEmailInput === "" || DesktopPasswordInput === "") {
+            DesktopShowCustomNotification( "لطقا فرم ها را کامل پر کنید");
+        } else if (isValidEmail(DesktopEmailInput) === false) {
+            DesktopShowCustomNotification("ایمیل وارد شده نا معتبر است !")
+        } else {
+            DesktopCheckData();
+        }
+    });
+
 
     function isValidEmail(email) {
         let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -65,27 +87,52 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 3000);
     }
 
+
+    function DesktopShowCustomNotification(message) {
+        let notification = document.getElementById('DesktopCustomNotification');
+        let notificationMessage = document.getElementById('DesktopNotificationMessage');
+
+        notificationMessage.textContent = message;
+        notification.classList.add('show-notification');
+
+        setTimeout(function () {
+            notification.classList.remove('show-notification');
+        }, 3000);
+    }
+
     let userData = [
         {email: 'parsa@gmail.com', password: '123'}
     ];
 
     function checkData() {
-
-
         let emailValue = emailInput.value;
         let passwordValue = passwordInput.value;
-
-        console.log(emailInput, passwordInput)
-
-
         let user = userData.find(user => user.email === emailValue && user.password === passwordValue);
 
         if (user) {
-            window.location.href = '../../pages/Tables.html';
+            MobileLinkButton.href = 'HomePage.html'
+            document.getElementById('EmailInput').value = "";
+            document.getElementById('PasswordInput').value = "";
         } else {
             showCustomNotification('اطلاعات وارد شده نامعتبر است');
         }
     }
+
+    function DesktopCheckData() {
+
+        let DesktopEmailValue = DesktopEmailInput.value;
+        let DesktopPasswordValue = DesktopPasswordInput.value;
+        let DesktopUser = userData.find(user => user.email === DesktopEmailValue && user.password === DesktopPasswordValue);
+
+        if (DesktopUser) {
+            DesktopLinkButton.href = 'HomePage.html'
+            document.getElementById('DesktopEmailInput').value = "";
+            document.getElementById('DesktopPasswordInput').value = "";
+        } else {
+            DesktopShowCustomNotification('اطلاعات وارد شده نامعتبر است');
+        }
+    }
+
 });
 
 
